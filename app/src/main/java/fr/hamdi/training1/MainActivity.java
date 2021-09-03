d71f464068f52a65de0ddffe499c7179e2c50ab8
abdAllah <abdelahamdi@gmail.com>
2021-09-03 20:20:37 +0200

    display username

--- app/src/main/java/fr/hamdi/training1/MainActivity.java
+++ app/src/main/java/fr/hamdi/training1/MainActivity.java
@@ -9,12 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    public final static String USER_NAME = null;
     private Button loginButton;
     private EditText emailEdit;
     private EditText passwordEdit;
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.login_button);
         emailEdit = findViewById(R.id.email_edit);
         passwordEdit = findViewById(R.id.password_edit);
-
+        userName = findViewById(R.id.user_edit);
         loginButton.setOnClickListener(new View.OnClickListener() {
 
 
@@ -34,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 String refpwd = passwordEdit.getText().toString();
 
 
+
                 if (refemail.equals("admin@admin.com") && refpwd.equals("admin")) {
                     Toast.makeText(MainActivity.this, getString(R.string.access_ok), Toast.LENGTH_SHORT).show();
                     Intent menuLogin = new Intent(getApplicationContext(), MenuLoginActivity.class);
+                    menuLogin.putExtra(USER_NAME,userName.getText().toString());
                     startActivity(menuLogin);
                     finish();
                 } else {
--- app/src/main/java/fr/hamdi/training1/MenuLoginActivity.java
+++ app/src/main/java/fr/hamdi/training1/MenuLoginActivity.java
@@ -2,13 +2,25 @@ package fr.hamdi.training1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MenuLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menulogin);
-    }
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.USER_NAME);
+        Toast.makeText(MenuLoginActivity.this, message, Toast.LENGTH_LONG).show();
+        System.out.println(message);
+
+        }
 }
