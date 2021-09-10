de9f981486117abb6c47d3b29191f67a9cdc775d
abdAllah <abdelahamdi@gmail.com>
2021-09-10 10:06:37 +0200

    display welcome username and football player images&names

--- app/src/main/java/fr/hamdi/training1/MenuLoginActivity.java
+++ app/src/main/java/fr/hamdi/training1/MenuLoginActivity.java
@@ -1,17 +1,20 @@
 package fr.hamdi.training1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class MenuLoginActivity extends AppCompatActivity {
 
-
+    String s1[];
+    int images[] = {R.drawable.benzema,R.drawable.griezman,R.drawable.hernandez,R.drawable.kante,R.drawable.kimpembe,R.drawable.lloris,R.drawable.matuidi,R.drawable.mbappe,R.drawable.mendy,R.drawable.pogba,R.drawable.tolisso};
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class MenuLoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menulogin);
+
+        s1 = getResources().getStringArray(R.array.football_players);
+        recyclerView = findViewById(R.id.recyclerView);
 
         TextView userNamelogged;
         userNamelogged = findViewById(R.id.user_name);
@@ -29,6 +35,12 @@ public class MenuLoginActivity extends AppCompatActivity {
 // Display the name in the activty menuLogin
         name = "Welcome "+name;
         userNamelogged.setText(name);
+
+        MyAdapter myAdapter = new MyAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
 
 
 
